e81d4a1f2b59c8506a750ff97d59674c20236c4d
droidmenow <g@g.com>
2021-01-01 16:53:17 +1100

    fix order

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   access_key                  = var.aws_access_key
+  secret_key                  = var.aws_secret_key
   region                      = var.aws_region
   s3_force_path_style         = true
-  secret_key                  = var.aws_secret_key
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
